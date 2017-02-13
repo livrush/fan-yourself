c24b35d70f97583edc3642fadf8078ae1d87e341
phrush <w.phrush@gmail.com>
2017-02-13 16:38:30 -0600

    Add shapes css, fix drop down menus so they don't dissapear when you select

--- code/client/controllers/UI.js
+++ code/client/controllers/UI.js
@@ -15,17 +15,35 @@ angular.module('UI.controller', [])
     'rhombus'
   ];
 
-  $scope.positions = [
-    'top left',
-    'top center',
-    'top right',
-    'center left',
-    'center center',
-    'center right',
-    'bottom left',
-    'bottom center',
-    'bottom right',
-  ];
+  // $scope.positions = [
+  //   'top left',
+  //   'top center',
+  //   'top right',
+  //   'center left',
+  //   'center center',
+  //   'center right',
+  //   'bottom left',
+  //   'bottom center',
+  //   'bottom right',
+  // ];
+
+  $scope.positions = {
+    1: 'top left',
+    2: 'top center',
+    3: 'top right',
+    4: 'center left',
+    5: 'center center',
+    6: 'center right',
+    7: 'bottom left',
+    8: 'bottom center',
+    9: 'bottom right'
+  };
+
+
+  $scope.test = function(data) {
+
+    console.log(data || 'You did it');
+  };
 });
 
 console.log('controller');
