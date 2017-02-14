baf2f6b49fcfd5d5649e1afe59b43c2ba9baa9f2
phrush <w.phrush@gmail.com>
2017-02-14 13:03:34 -0600

    Create better color input visualizer

--- code/client/app/userInterface/services.js
+++ code/client/app/userInterface/services.js
@@ -9,14 +9,14 @@ angular.module('logoer.services', [])
       if (input.length === 6) {
         if (input[0] !== '#') {
           document.getElementById('hexColor').value = `#${input}`;
-          document.getElementById('hexColor').style.background = input;
-          document.getElementById('textColor').style.background = input;
+          document.getElementById('hexColor').style['background-color'] = input;
+          document.getElementById('textColor').style['background-color'] = input;
           changer('#' + colors.text());
         }
       } else if (input.length === 7) {
         document.getElementById('hexColor').value = input;
-        document.getElementById('hexColor').style.background = input;
-        document.getElementById('textColor').style.background = input;
+        document.getElementById('hexColor').style['background-color'] = input;
+        document.getElementById('textColor').style['background-color'] = input;
       }
     }
   };
